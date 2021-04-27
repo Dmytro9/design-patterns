4547d9a0f4f12126eb67f67750cf1f876231576d
dpoliu <dpoliuliuk@atlassian.com>
2021-04-27 11:36:30 +0300

    1

---
+++ index.js
@@ -0,0 +1,25 @@
+/*  CREATIONAL:
+
+    Constructor - class -> constructor(){}
+    Factory - class -> create(){}
+    Prototype - Object.create(objProt, {})
+    Singeltone - in app only one instance of this class (db)
+
+
+
+
+    STRUCTURAL:
+
+    Adapter - old interface with new methods
+    Decorator - modify / extend class and return it
+    Facade - public interface for simplifying communication between classes
+    Flyweight - 
+    Proxy - new Proxy(fn, {})
+
+
+
+    BEHAVIOR: (communication between objects of different types)
+
+    Chain-of-responsability - in class return this and can chain methods
+    Command - 
+**/
